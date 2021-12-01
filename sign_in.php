da328a1d8cbb954de573518b4e47fbdc7cdde3d8
Nikita <nikita.motlitch@yandex.ru>
2021-12-01 21:22:52 +0300

    Добавил файл обработчик входа

---
+++ sign_in.php
@@ -0,0 +1,34 @@
+<?php
+
+require "auth/utils.php";
+redirectIfUserLogged();
+
+header("Content-Type: application/json");
+
+require_once "validation/sign_up_validation.php";
+require_once "models/models.php";
+require_once "validation/sign_in_validation.php";
+
+$data = $_POST;
+$validation_obj = new FormValidator($sign_in_rules, $sign_in_messages, $data);
+$validation_obj->validate();
+
+$errors = $validation_obj->getErrorMessages();
+
+if (empty($errors)) {
+    $user_obj = new User();
+    $user = $user_obj->getUserIfPasswordVerify($data['email'], $data['password']);
+
+    if ($user) {
+        session_start();
+        $_SESSION['user_id'] = $user['user_id'];
+    } else {
+        array_push($errors, "Неверный логин или пароль");
+    }
+}
+
+if (!empty($errors)) {
+    echo json_encode(["errors" => $errors]);
+} else {
+    echo json_encode(["response" => "OK"]);
+}
