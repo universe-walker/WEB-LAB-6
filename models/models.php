095e6ab0c5e8d9cd9c0b250594c440e0cd042889
Nikita <nikita.motlitch@yandex.ru>
2021-12-01 22:46:16 +0300

    Убрал из класса Comment переменную класса post_id, метод setPostId. Добавил в методе getAllActiveComments аргумент post_id

--- models/models.php
+++ models/models.php
@@ -38,14 +38,8 @@ class Post extends DB
 
 class Comment extends DB
 {
-    private $post_id = null;
-
-    public function getAllActiveComments()
+    public function getAllActiveComments($post_id)
     {
-        if (is_null($this->post_id)) {
-            throw new Exception("post_id не установлен", 1);
-        }
-
         $sth = $this->dbh->prepare(
             "SELECT id, text, date_time, is_active
             FROM comment AS C
@@ -53,20 +47,9 @@ class Comment extends DB
             WHERE P.post_id=:post_id AND C.is_active
         "
         );
-        $sth->bindValue(":post_id", $this->post_id, PDO::PARAM_INT);
+        $sth->bindValue(":post_id", $post_id, PDO::PARAM_INT);
         $sth->execute();
         return $sth;
-    }
-
-    public function setPostId($id)
-    {
-        if (!is_int($id)) {
-            throw new Exception("post_id должен быть типом integer", 1);
-        }
-        if ($id < 1) {
-            throw new Exception("id должен быть больше или равен 1", 1);
-        }
-        $this->post_id = $id;
     }
 }
 
@@ -101,8 +84,7 @@ class User extends DB
     public function getUserIfPasswordVerify($email, $password)
     {
         $user = $this->getUserByEmail($email);
-        if ($user)
-        {
+        if ($user) {
             if (password_verify($password, $user['password_hash'])) {
                 return $user;
             }
