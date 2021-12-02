5a197ca622602bb7c79336f7b27e12ffecccd96d
Nikita <nikita.motlitch@yandex.ru>
2021-12-02 13:15:25 +0300

    Добавил вывод приветствия для пользователя, если пользователь авторизован

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -9,9 +9,17 @@
       </div>
     </div>
     <div class="nav__right">
+      <?php if ($user): ?>
+      <div class="nav__hello nav_item">Привет,&#32;<span class="nav__username"><?= $user['name'] ?></span></div>
+      <div class="nav__logout nav_item">
+        <a href=<?= "/logout.php?next=" . $_SERVER['REQUEST_URI'] ?>
+          class="nav__logout nav_link link">Выйти</a>
+      </div>
+      <?php else: ?>
       <div class="nav__login nav_item">
         <a href="#" id="login_link" class="nav_link link">Войти/Регистрация</a>
       </div>
+      <?php endif; ?>
     </div>
   </div>
 </nav>
