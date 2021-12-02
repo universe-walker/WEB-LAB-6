7ae8fcf90453ebacd7c1c72246e05fe96e7a4c05
Nikita <nikita.motlitch@yandex.ru>
2021-12-02 23:09:07 +0300

    Поменял name у textarea, добавил скрытое поле с post_id

--- views/layouts/comment_form.php
+++ views/layouts/comment_form.php
@@ -1,8 +1,9 @@
 <div class="comment__comment-form-wrapper">
-    <form action="#" method="GET" class="comment__comment-form">
+    <form action="#" method="POST" class="comment__comment-form">
         <label for="comment">Введите ваш комментарий:</label>
-        <textarea name="textarea" class="comment-form__input" data-validate-field="comment" id="comment" cols="50"
+        <textarea name="text" class="comment-form__input" data-validate-field="comment" id="comment" cols="50"
             rows="10"></textarea>
-        <button class="comment-form__submit" name="submit" value=<?= $_GET['post-id'] ?>>Отправить</button>
+        <input type="hidden" name="post_id" value=<?= $_GET['post-id'] ?>>
+        <button class="comment-form__submit" name="submit">Отправить</button>
     </form>
 </div>
