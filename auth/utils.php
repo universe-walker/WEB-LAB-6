4b66fccb5968295138cf3dfe4e608b9dd1a1b5a3
Nikita <nikitamotlich@gmail.com>
2022-01-27 19:27:42 +0300

    Рефакторинг. Изменил способ проверки наличия элементов в массиве

--- auth/utils.php
+++ auth/utils.php
@@ -4,7 +4,7 @@ function redirectIfUserLogged($user_var='user_id', $redirect_to='/')
 {
     session_start();
 
-    if ($_SESSION[$user_var]) {
+    if (array_key_exists($user_var, $_SESSION)) {
         header("Location: " . $redirect_to);
     }
 }
@@ -13,7 +13,7 @@ function getUserIfUserLogged()
 {
     session_start();
 
-    if ($_SESSION['user_id']) {
+    if (array_key_exists('user_id', $_SESSION)) {
         $user_id = (int) $_SESSION['user_id'];
 
         require_once "models/models.php";
@@ -28,8 +28,8 @@ function getUserIfUserLogged()
 function redirectIfUserNotLogged($user_var='user_id', $redirect_to='/')
 {
     session_start();
-
-    if (!$_SESSION[$user_var]) {
+    
+    if (!array_key_exists($user_var, $_SESSION)) {
         header("Location: " . $redirect_to);
     }
 }
