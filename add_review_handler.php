a8745f68477e9cd29da1cf760cbb7fdb746e5f85
Nikita <nikitamotlich@gmail.com>
2022-02-19 22:06:51 +0300

    Починил слияние ошибок валидации

--- add_review_handler.php
+++ add_review_handler.php
@@ -15,7 +15,7 @@ $validator = new ReviewValidator();
 $errors = $validator->validate($_POST);
 
 $validator_poster = new FileValidator('poster', ['image/jpeg', 'image/png'], 1);
-$errors += $validator_poster->validate();
+$errors = array_merge($errors, $validator_poster->validate());
 
 
 if (!empty($errors)) {
@@ -23,6 +23,7 @@ if (!empty($errors)) {
     $_SESSION['errors'] = $errors;
 
     header('Location: add_review.php');
+    die();
 }
 
 $path_info = pathinfo($_FILES['poster']['name']);
@@ -31,7 +32,7 @@ $new_path = 'upload' . "/" . uniqid() . "." . $ext;
 $from = $_FILES['poster']['tmp_name'];
 
 // получаем код встроенного видео
-$url_youtube = "https://www.youtube.com/oembed?url=" . $_POST['trailer_code'] . "&format=json&maxwidth=560&maxheight=315";
+$url_youtube = "https://www.youtube.com/oembed?url=" . $_POST['trailer_link'] . "&format=json&maxwidth=560&maxheight=315";
 $ch = curl_init($url_youtube);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = json_decode(curl_exec($ch), true);
